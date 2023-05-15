e3ccdd31bebae51535e33d6e9e399c7965fcde5e
rosercode <rosercode@gmail.com>
2023-05-15 16:28:00 +0800

    feat project init

---
+++ src/test/java/cool/example/plmn/AppTest.java
@@ -0,0 +1,18 @@
+package cool.example.plmn;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() throws SQLException {
+
+    }
+}
